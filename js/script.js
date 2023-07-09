b0bcfc06a9bd61adeca48204ee66151c06fb310b
Gina Cahya Utami <ginacu12@gmail.com>
2023-07-09 11:08:34 -0700

    berhasil menampilkan seluruh data product

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $.getJSON('data/products.json', function(data) {
     let products = data.products;
     // method pengulangan(looping) pada jquery dinamakan $.each
     $.each(products, function (i, data) {
-        
-    })
+        // arti kode di bawah adalah: jquery carikan saya elemen yang id nya daftar-menu lalu tambahkan di akhir(append) sebuah elemen baru (string html pembuat card)
+        $('#daftar-menu').append('<div class="col-md-4">        <div class="card mb-3"><img src="'+ data.images[0] + '"class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.title + '/h5><p class="card/text">' + data.description + '</p><h5 class="card-title">' + data.price + '</h5><a href="#" class="btn btn-primary">Order Now</a></div></div></div>')
+    });
 });
