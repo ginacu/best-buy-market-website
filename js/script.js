5b146d9f8b77d52a0569fb9058cf09ae34e24bca
Gina Cahya Utami <ginacu12@gmail.com>
2023-07-24 09:39:58 -0700

    membuat fungsi tampilkanSemuaProduk() agar tidak berulang membuat #daftar-produk nya

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // Mengambil data JSON menggunakan AJAX
 
-$.getJSON('data/products.json', function(data) {
-    // membuat variabel products untuk mengilangkan key 'products'nya
-    let products = data.products;
-    // method pengulangan(looping) pada jquery dinamakan $.each
-    $.each(products, function (i, data) {
-        // arti kode di bawah adalah: jquery carikan saya elemen yang id nya daftar-menu lalu tambahkan di akhir(append) sebuah elemen baru (string html pembuat card)
-        $('#daftar-menu').append('<div class="col-md-4">        <div class="card mb-3"><img src="'+ data.images[0] + '"class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.title + '/h5><p class="card/text">' + data.description + '</p><h5 class="card-title">' + data.price + '</h5><a href="#" class="btn btn-primary">Order Now</a></div></div></div>')
+function tampilkanSemuaProduk() {
+    $.getJSON('data/products.json', function(data) {
+        // membuat variabel products untuk mengilangkan key 'products'nya
+        let products = data.products;
+        // method pengulangan(looping) pada jquery dinamakan $.each
+        $.each(products, function (i, data) {
+            // arti kode di bawah adalah: jquery carikan saya elemen yang id nya daftar-produk lalu tambahkan di akhir(append) sebuah elemen baru (string html pembuat card)
+            $('#daftar-produk').append('<div class="col-md-4">        <div class="card mb-3"><img src="'+ data.images[0] + '"class="card-img-top"><div class="card-body"><h5 class="card-title">' + data.title + '/h5><p class="card/text">' + data.description + '</p><h5 class="card-title">' + data.price + '</h5><a href="#" class="btn btn-primary">Order Now</a></div></div></div>')
+        });
     });
-});
+}
+
+tampilkanSemuaProduk()
 
 $('.nav-link').on('click', function() {
     // hapus semua kelas 'active' pada kelas nav-link
@@ -32,7 +36,7 @@ $('.nav-link').on('click', function() {
             }
         });
 
-        $('#daftar-menu').html(content);
+        $('#daftar-produk').html(content);
 
     });
 
